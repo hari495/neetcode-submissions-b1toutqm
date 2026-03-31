9dd8595c27dc14218509cabb7c758d1a41cfe3e8
Hari Kolla <harikolla78@gmail.com>
2026-03-31 14:41:56 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        map_s={}
+        map_t={}
+        if len(s)!=len(t):
+            return False
+        for i in range(len(s)):
+            map_s[s[i]]=map_s.get(s[i],0)+1
+            map_t[t[i]]=map_t.get(t[i],0)+1
+        
+        return map_s==map_t
+        
